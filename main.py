25558c0081a0ea69d70c888d9fbc89897cf361e9
Ahmed Numan <reduanahmed773@gmail.com>
2025-11-13 20:57:31 +0600

    Fixed: Plugin not loading

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
+# Copyright (c) 2025 devgagan : https://github.com/devgaganin.
+# Licensed under the GNU General Public License v3.0.
 # See LICENSE file in the repository root for full license text.
-
-
-
-
-
 
 import asyncio
 import importlib
@@ -16,18 +11,6 @@ from pyrogram import idle
 from shared_client import start_client, client, app, userbot
 
 
-# async def load_and_run_plugins():
-#     await start_client()
-#     plugin_dir = "plugins"
-#     plugins = [f[:-3] for f in os.listdir(plugin_dir) if f.endswith(".py") and f != "__init__.py"]
-
-#     for plugin in plugins:
-#         module = importlib.import_module(f"plugins.{plugin}")
-#         if hasattr(module, f"run_{plugin}_plugin"):
-#             print(f"Running {plugin} plugin...")
-#             await getattr(module, f"run_{plugin}_plugin")()  
-
-# updated plugin function
 async def load_and_run_plugins():
     """
     plugins ফোল্ডারের সব .py ফাইল import করবে,
@@ -57,27 +40,6 @@ async def load_and_run_plugins():
         except Exception as e:
             print(f"❌ Failed to load plugin {plugin}: {e}")
 
-
-# async def main():
-#     await load_and_run_plugins()
-#     while True:
-#         await asyncio.sleep(1)  
-
-# if __name__ == "__main__":
-#     loop = asyncio.get_event_loop()
-#     print("Starting clients ...")
-#     try:
-#         loop.run_until_complete(main())
-#     except KeyboardInterrupt:
-#         print("Shutting down...")
-#     except Exception as e:
-#         print(e)
-#         sys.exit(1)
-#     finally:
-#         try:
-#             loop.close()
-#         except Exception:
-#             pass
 
 async def main():
     print("Starting clients ...")
@@ -109,6 +71,7 @@ async def main():
         await client.disconnect()
     except Exception:
         pass
+
 
 if __name__ == "__main__":
     try:
